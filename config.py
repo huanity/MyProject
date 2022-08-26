fd5c7288505690b1faade960da7660667cb78912
huanity <huanity@foxmail.com>
2022-08-26 20:33:31 +0800

    增加基础配置

---
+++ config.py
@@ -0,0 +1,6 @@
+# -*- coding:utf-8 -*-
+"""
+        File Name     :    config.py.py
+        Create Date   :    2022-08-26    19:07
+"""
+USER_NAME = "Admin"
--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("hello,world!")
+from config import *
+
+
+def main():
+    print(f"{USER_NAME},hello,world!")
+
+
+if __name__ == '__main__':
+    main()
